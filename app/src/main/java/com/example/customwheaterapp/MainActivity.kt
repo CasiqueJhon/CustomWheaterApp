bb42b582791b6a27516f06943187c73d73149d5b
Jhon Casique <jhoncasique04@hotmail.com>
2021-01-17 00:45:36 +0100

    feat:[RESOURCES] Add multidex and add weather images

--- app/src/main/java/com/example/customwheaterapp/MainActivity.kt
+++ app/src/main/java/com/example/customwheaterapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.customwheaterapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.multidex.MultiDex
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
 }
