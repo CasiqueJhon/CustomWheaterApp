ec6123ef2b71a5f107d784278327a7b855e1d711
Jhon Casique <jhoncasique04@hotmail.com>
2021-01-18 23:25:30 +0100

    fear: change to ClimaCell API v4

--- app/src/main/java/com/example/customwheaterapp/utils/Constant.kt
+++ app/src/main/java/com/example/customwheaterapp/utils/Constant.kt
@@ -3,8 +3,8 @@ package com.example.customwheaterapp.utils
 class Constant {
 
     companion object {
-        const val APY_KEY = "8db117984196e55d229a1f03bfa83250"
-        const val BASE_URL = "http://api.openweathermap.org/"
+        const val APY_KEY = "2kLhCxSczp68HHv9Fn6YpVme7aBxWEbn"
+        const val BASE_URL = "https://data.climacell.co/v4"
         const val AppId = "2e65127e909e178d0af311a81f39948c"
         const val lat = "40.2508572"
         const val lon = "-3.7018663"
